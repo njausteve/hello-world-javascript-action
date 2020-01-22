b6426819753faefdd15ec9894d9067deac0c5847
stephen <njaustevedomino@gmail.com>
2020-01-22 21:24:59 +0800

    print branch Name

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ const github = require("@actions/github");
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
-  const branchName = core.getInput("branch-name");
   const time = new Date().toTimeString();
+
+  const branchName = core
+    .getInput("prefix")
+    .split(",")
+    .map(prefix => prefix.trim())
+    .reduce((acc, curr) => acc.replace(curr, ""), process.env.GITHUB_HEAD_REF);
+
+  console.log(`branch-name: ${branchName}`);
 
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   core.setOutput("time", time);
 
   console.log(`Hello ${nameToGreet}!`);
-  console.log(`branch-name: ${branchName}`);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
